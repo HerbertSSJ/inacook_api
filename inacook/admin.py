42836c728ea0e3baba8d1bbac6fc6737c6192787
HerbertSSJ <herberturtubia@hotmail.com>
2025-12-03 05:27:16 -0300

    modelos
    se añadieron todos los modelos

--- inacook/admin.py
+++ inacook/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import rol, usuario
+from .models import rol, usuario, unidadmedicion, ingrediente, receta, receta_ingrediente, comprobante, historial
 
 # Register your models here.
 admin.site.register(rol)
 admin.site.register(usuario)
+admin.site.register(unidadmedicion)
+admin.site.register(ingrediente)
+admin.site.register(receta)
+admin.site.register(receta_ingrediente)
+admin.site.register(comprobante)
+admin.site.register(historial)
--- inacook/models.py
+++ inacook/models.py
@@ -16,3 +16,56 @@ class usuario (models.Model):
     def __str__(self):
         return self.user.username
 
+class unidadmedicion(models.Model):
+    nombre=models.CharField(max_length=45)
+    abreviatura=models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.nombre} ({self.abreviatura})"
+
+class ingrediente(models.Model):
+    nombre=models.CharField(max_length=80)
+    calidad=models.CharField(max_length=45)
+    costo_unitario=models.DecimalField()
+    unidad_medicion=models.ForeignKey(unidadmedicion, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.nombre
+    
+    class receta(models.Model):
+        nombre=models.CharField(max_length=100)
+        categoria=models.CharField(max_length=45)
+        aporte_calorico=models.IntegerField(null=True, blank=True)
+        tiempo_preparacion=models.IntegerField(null=True, blank=True)
+        usuario=models.ForeignKey(usuario, null=True, on_delete=models.SET_NULL)
+
+        def __str__(self):
+            return self.nombre
+        
+class receta_ingrediente(models.Model):
+    receta=models.ForeignKey(receta, on_delete=models.CASCADE)
+    ingrediente=models.ForeignKey(ingrediente, on_delete=models.CASCADE)
+    cantidad=models.FloatField()
+
+    def __str(self):
+        return f"{self.cantidad} de {self.ingrediente.nombre}"
+    
+    class comprobante(models.Model):
+        receta=models.ForeignKey(receta, null=True, on_delete=models.SET_NULL)
+        factor_multiplicacion=models.IntegerField(default=1)
+        iva=models.IntegerField(default=19)
+        precio_bruto=models.IntegerField(default=0)
+        fecha=models.DateField(null=True, blank=True)
+
+        def __str__(self):
+            return f"comprobante #{self.id}"
+        
+        class historial(models.Model):
+            receta=models.ForeignKey(receta, on_delete=models.CASCADE)
+            usuario=models.ForeignKey(usuario, null=True, on_delete=models.SET_NULL)
+            fecha_entrega=models.DateField(null=True, blank=True)
+            fecha_modificacion=models.DateTimeField(auto_now=True)
+            cambio_realizado=models.TextField(null=True, blank=True)
+
+            def __str__(self):
+                return f"historial {self.id}"
