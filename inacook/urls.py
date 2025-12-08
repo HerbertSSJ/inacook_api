2d801273df216a35a1731c20532c23c1a35fc230
Daniel3405 <d.vilchez0304@gmail.com>
2025-12-08 00:46:21 -0300

    views.py avance

--- inacook/urls.py
+++ inacook/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from inacook.views import (ListaIngredientes, DetalleIngrediente)
+from inacook.views import (ListaIngredientes, DetalleIngrediente,ListaRoles,DetalleRol, ListaUnidadMedicion, DetalleUnidadMedicion)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('ingredientes/', ListaIngredientes.as_view()),
     path('ingredientes/<int:id>/', DetalleIngrediente.as_view()),
+    path('roles/',ListaRoles.as_view()),
+    path('roles/<int:id>/', DetalleRol.as_view()),
+    path('unidades/', ListaUnidadMedicion.as_view()),
+    path('unidades/<int:id>/', DetalleUnidadMedicion.as_view()),
 ]
